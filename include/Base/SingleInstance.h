d2c05a1bd1df6f1c8c09d585aa769b5bde77f062
nor-s <nors.nsg@gmail.com>
2025-02-20 08:00:48 +0900

    base: add singleInstance class for singleton

---
+++ include/Base/SingleInstance.h
@@ -0,0 +1,31 @@
+#ifndef SGLIB_SINGLEINSTANCE_H
+#define SGLIB_SINGLEINSTANCE_H
+
+namespace sglib
+{
+
+template <typename T>
+class SingleInstance
+{
+protected:
+	SingleInstance() = default;
+
+public:
+	SingleInstance(SingleInstance<T>&&) = delete;
+	SingleInstance(const SingleInstance<T>&) = delete;
+
+	SingleInstance<T>& operator=(const SingleInstance<T>&) = delete;
+	SingleInstance<T>& operator=(SingleInstance<T>&&) = delete;
+
+	~SingleInstance() = default;
+
+	static T& get()
+	{
+		static T s_instance;
+		return s_instance;
+	}
+};
+
+}	 // namespace sglib
+
+#endif
