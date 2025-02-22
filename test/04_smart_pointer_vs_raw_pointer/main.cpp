88a6fd74e49bd0cd3ae33ef64957820653e9665a
nor-s <nors.nsg@gmail.com>
2025-02-22 12:09:04 +0900

    test: add smart pointer vs raw pointer

---
+++ test/04_smart_pointer_vs_raw_pointer/main.cpp
@@ -0,0 +1,108 @@
+#include <sglib.h>
+#include <memory>
+
+#include <chrono>
+#include <vector>
+
+class Object
+{
+public:
+	Object() = default;
+	~Object() = default;
+	void DoSomething()
+	{
+		value++;
+	}
+
+private:
+	int value = 0;
+};
+
+constexpr int TEST_COUNT = 1000000;
+
+void TestUniquePtr()
+{
+	std::vector<std::unique_ptr<Object>> objects;
+	objects.reserve(TEST_COUNT);
+
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (int i = 0; i < TEST_COUNT; ++i)
+		{
+			objects.push_back(std::make_unique<Object>());
+		}
+	}
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (auto& obj : objects)
+		{
+			obj->DoSomething();
+		}
+	}
+}
+
+void TestSharedPtr()
+{
+	std::vector<std::shared_ptr<Object>> objects;
+	objects.reserve(TEST_COUNT);
+
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (int i = 0; i < TEST_COUNT; ++i)
+		{
+			objects.push_back(std::make_shared<Object>());
+		}
+	}
+
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (auto& obj : objects)
+		{
+			obj->DoSomething();
+		}
+	}
+}
+
+void TestRawPtr()
+{
+	std::vector<Object*> objects;
+	objects.reserve(TEST_COUNT);
+
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (int i = 0; i < TEST_COUNT; ++i)
+		{
+			objects.push_back(new Object());
+		}
+	}
+
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (auto obj : objects)
+		{
+			obj->DoSomething();
+		}
+	}
+
+	{
+		sglib::timer::PerformanceTimer timer{{}};
+		for (auto obj : objects)
+		{
+			delete obj;
+		}
+	}
+}
+
+int main()
+{
+	sglib::Logger::SetLogPrefix(TEXT("04_smart_pointer_vs_raw_pointer"));
+
+	SG_LOG_WARN(TEXT("TestUniquePtr (cons, call member function)"));
+	sglib::timer::InvokeWithTimer({}, TestUniquePtr);
+
+	SG_LOG_WARN(TEXT("TestSharedPtr (cons, call member function)"));
+	sglib::timer::InvokeWithTimer({}, TestSharedPtr);
+
+	SG_LOG_WARN(TEXT("TestRawPtr (new, call member function, delete)"));
+	sglib::timer::InvokeWithTimer({}, TestRawPtr);
+}
